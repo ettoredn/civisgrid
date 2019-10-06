cdf4ff501e4cf7492806cac0e84a90bff290236e
Ettore Del Negro <ettore@themecraft.studio>
2019-10-07 01:11:47 +0200

    sync

--- src/bin/doubleauction.rs
+++ src/bin/doubleauction.rs
@@ -1,19 +1,19 @@
+#![allow(dead_code)]
+#![allow(unused)]
+
 /**
  * https://www.ralfj.de/blog/2019/07/14/uninit.html
  */
-
 use std::thread::{self, JoinHandle};
-use std::mem::{self, MaybeUninit, drop};
 use std::sync::mpsc::{self, channel, Sender};
+use rand::distributions::{Uniform};
+use rand::prelude::*;
+use std::time::Duration;
 
-const AGENTS: usize = 10;
-
-// const AGENT: fn() -> () = || {
-//     println!("Agent!");
-// };
+const AGENTS: usize = 5;
 
 fn main() {
-    println!("Double auction market");
+    let price_distr = Uniform::new_inclusive(1, 10);
 
     let _order_counter = 0; // This should be shared across threads
 
@@ -26,16 +26,29 @@ fn main() {
         let send_order = send_order.clone();
 
         let handle = thread::Builder::new().name(format!("Agent {}", i)).spawn(move || {
-            if let Some(name) = thread::current().name() {
-                println!("{} Hey there!", name);
+            let mut rng = thread_rng();
+
+            loop {
+                thread::sleep(Duration::from_secs(rng.gen_range(1,5)));
+
+                let price = rng.sample(price_distr) as f32;
+
+                let side = match rng.gen::<bool>() {
+                    true => OrderSide::Ask,
+                    false => OrderSide::Bid
+                };
+
+                send_order.send(LimitOrder{
+                    quantity: 2,
+                    price: price,
+                    side: side,
+                    id: 123
+                }).unwrap();
             }
-            
-            send_order.send(LimitOrder{price: 1.1, quantity: 2, side: OrderSide::Ask, id: 123});
         }).unwrap();
 
         agents.push(handle);
     }
-
     
     let daex = thread::Builder::new().name("DAEX".to_string()).spawn(move || {
         while let Ok(order) = order_receiver.recv() {
