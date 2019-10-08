8d611e1c92c36439d78d9ecf6340f233db4ab425
Ettore Del Negro <ettore@themecraft.studio>
2019-10-08 19:29:18 +0200

    sync

--- src/bin/crypto_order_flow.rs
+++ src/bin/crypto_order_flow.rs
@@ -3,6 +3,7 @@
 
 use websocket_lite::{ClientBuilder, Message, Opcode};
 use serde::{Deserialize, Serialize};
+use serde_json::{Value};
 
 fn main() {
     let mut client = ClientBuilder::new("wss://ws.bitstamp.net")
@@ -11,23 +12,62 @@ fn main() {
         .expect("Couldn't connect to WS stream");
 
 
-    let sub_mess = Message::new(Opcode::Text, r#"{
+    let sub_mess: Message = Message::text(r#"{
         "event": "bts:subscribe",
         "data": {
             "channel": "live_orders_btceur"
         }
-    }"#.to_string()).unwrap();
+    }"#);
 
     client.send(sub_mess);
 
     loop {
+        // Framed<S: Read, C: Decoder>::receive() @ https://docs.rs/websocket-lite/0.2.4/src/websocket_lite/sync.rs.html
         let message = match client.receive() {
             Ok(Some(message)) => message,
-            Ok(None) | Err(_) => { break; }
+            // If the bytes look valid, but a frame isn't fully available yet, then Ok(None) is returned.
+            Ok(None) => { 
+                println!("client.receive() == Ok(None)");
+                continue;
+            }
+            Err(_) => break
         };
 
-        let text = message.as_text().unwrap();
-        dbg!(&text);
+        // Ok(None) because I am not answering to the ping!!
+
+        match message.opcode() {
+            Opcode::Ping => { println!("Opcode::Ping") },
+            Opcode::Binary => { println!("Opcode::Binary") },
+            Opcode::Text => {
+                let text = message.as_text().unwrap();
+                let mut v: Value = serde_json::from_str(text).unwrap();
+
+                let event: &Value = v.get("event").unwrap();
+
+                if let Some(event) = v.get("event") {
+                    match event.as_str() {
+                        Some("bts:subscription_succeeded") => {
+                            println!("Subscribed");
+                        },
+                        Some("order_created") => {
+                            let v = v.get_mut("data").unwrap().take();
+                            let order: Order = serde_json::from_value(v).unwrap();
+
+                            println!("ORDER CREATED: {:?}", order);
+                        },
+                        Some("order_changed") => { },
+                        Some("order_deleted" ) => { },
+                        Some(event) => {
+                            panic!("Unknown event {}", event);
+                        }
+                        None => {}
+                    }
+                }
+            },
+
+            Opcode::Close => { println!("Opcode::Close") },
+            _ => ()
+        }
 
         // let o: Order = dbg!(serde_json::from_str(text).unwrap());
     }
@@ -35,18 +75,12 @@ fn main() {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Order {
-    channel: String,
-    event: String,
-    data: OrderData
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct OrderData {
-    microtimestamp: u64,
+    microtimestamp: String,
     amount: f32,
     order_type: u8,
     amount_str: String,
     price_str: String,
     price: f32,
     id: u64,
-    datetime: u64,
+    datetime: String,
 }
--- src/bin/doubleauction.rs
+++ src/bin/doubleauction.rs
@@ -1,19 +1,27 @@
+/*
+ * https://medium.com/lgogroup/a-matching-engine-for-our-values-part-2-60b71c9eef26 
+ * https://medium.com/lgogroup/a-matching-engine-for-our-values-part-1-795a29b400fa 
+ * https://www.ralfj.de/blog/2019/07/14/uninit.html
+ * Crypto exchanges providing order flow:
+ *  - Coinbase https://docs.pro.coinbase.com/#the-full-channel
+ *  - Bitstamp https://www.bitstamp.net/websocket/v2/
+ * Coinbase Pro operates a continuous first-come, first-serve order book. Orders are executed in price-time priority as received by the matching engine.
+ */
 #![allow(dead_code)]
 #![allow(unused)]
 
-/**
- * https://www.ralfj.de/blog/2019/07/14/uninit.html
- */
 use std::thread::{self, JoinHandle};
 use std::sync::mpsc::{self, channel, Sender};
 use rand::distributions::{Uniform};
 use rand::prelude::*;
 use std::time::Duration;
+use std::collections::{btree_map::BTreeMap, vec_deque::VecDeque};
 
 const AGENTS: usize = 5;
 
 fn main() {
     let price_distr = Uniform::new_inclusive(1, 10);
+    let order_delay = Uniform::new(1, 5);
 
     let _order_counter = 0; // This should be shared across threads
 
@@ -29,9 +37,9 @@ fn main() {
             let mut rng = thread_rng();
 
             loop {
-                thread::sleep(Duration::from_secs(rng.gen_range(1,5)));
+                thread::sleep(Duration::from_secs(order_delay.sample(&mut rng)));
 
-                let price = rng.sample(price_distr) as f32;
+                let price = rng.sample(price_distr);
 
                 let side = match rng.gen::<bool>() {
                     true => OrderSide::Ask,
@@ -40,9 +48,10 @@ fn main() {
 
                 send_order.send(LimitOrder{
                     quantity: 2,
-                    price: price,
+                    price: price as f32,
                     side: side,
-                    id: 123
+                    id: 123,
+                    all_or_none: true,
                 }).unwrap();
             }
         }).unwrap();
@@ -51,8 +60,29 @@ fn main() {
     }
     
     let daex = thread::Builder::new().name("DAEX".to_string()).spawn(move || {
+        // Instantiates the order book
+        let mut book = OrderBook{
+            ask: OrderBookSide{
+                side: OrderSide::Ask,
+                entries: BTreeMap::new(),
+            },
+            bid: OrderBookSide{
+                side: OrderSide::Ask,
+                entries: BTreeMap::new()
+            }
+        };
+
         while let Ok(order) = order_receiver.recv() {
             println!("[DAEX] Received order {:?}", order);
+
+            match book.match_order(&order) {
+                Some(matched_order) => {
+                    println!("[DAEX] Matched order {:?} with {:?}", order, matched_order);
+                },
+                None => {
+                    book.add_order(order);
+                }
+            }
         }
     }).unwrap();
 
@@ -74,8 +104,100 @@ struct LimitOrder {
     quantity: usize,
     side: OrderSide,
     id: usize,
+    all_or_none: bool,
 }
 #[derive(Debug)]
 struct OrderExecuted {
     order: LimitOrder
 }
+#[derive(Debug)]
+struct OrderBook {
+    bid: OrderBookSide,
+    ask: OrderBookSide,
+}
+impl OrderBook {
+    /**
+     * Returns the matched order removed from the book, if any.
+     */
+    fn match_order(&mut self, order: &LimitOrder) -> Option<LimitOrder> {
+        let price = order.price;
+
+        let matching_side = match order.side {
+            OrderSide::Ask => &mut self.ask,
+            OrderSide::Bid => &mut self.bid
+        };
+
+        matching_side.match_order(order)
+    }
+
+    fn add_order(&mut self, order: LimitOrder) {
+        let side = match order.side {
+            OrderSide::Ask => &mut self.ask,
+            OrderSide::Bid => &mut self.bid,
+        };
+
+        side.add_order(order);
+    }
+}
+
+#[derive(Debug)]
+struct OrderBookSide {
+    side: OrderSide,
+    entries: BTreeMap<u32, OrderBookLevel>
+}
+impl OrderBookSide {
+    fn add_order(&mut self, order: LimitOrder) {
+        let price_level = OrderBookSide::get_order_level(&order);
+
+        if !self.entries.contains_key(&price_level) {
+            let entry = OrderBookLevel{
+                orders: VecDeque::new(),
+                price: price_level,
+            };
+
+            self.entries.insert(price_level, entry);
+        }
+
+        let level = self.entries.get_mut(&price_level).unwrap();
+
+        println!("[DAEX] Adding order level={} side={:?}: {:?}", price_level, self.side, order);
+        level.add_order(order);
+    }
+
+    fn get_order_level(order: &LimitOrder) -> u32 {
+        (order.price * 100.0).trunc() as u32
+    }
+
+    fn match_order(&mut self, order: &LimitOrder) -> Option<LimitOrder> {
+        let level = OrderBookSide::get_order_level(order);
+
+        match self.entries.get_mut(&level) {
+            Some(level) => level.match_order(&order),
+            _ => None
+        }
+    }
+}
+#[derive(Debug)]
+struct OrderBookLevel {
+    orders: VecDeque<LimitOrder>,
+    price: u32,
+}
+impl OrderBookLevel {
+    fn match_order(&mut self, order: &LimitOrder) -> Option<LimitOrder> {
+        if let Some(front_order) = self.orders.front() {
+            assert_eq!(front_order.price, order.price);
+
+            return self.orders.pop_front()
+        }
+
+        None
+    }
+
+    fn add_order(&mut self, order: LimitOrder) {
+        // TODO Check for order duplication? maybe on the upper context
+        println!("[DAEX/Book] Added order level={}: {:?}", self.price, order);
+
+        self.orders.push_back(order);
+    }
+}
+
